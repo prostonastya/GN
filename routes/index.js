3bc2c2a5259cca820206102512933670033b6d41
Anna Maksymenko <anna.maksymenko@nure.ua>
2017-08-21 11:37:42 +0300

    applyed es2015 standarts

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,5 @@ let db = require('../queries');
 
 router.get('/api/locations', db.getLocations);
 router.get('/api/locations/:id', db.getSingleLocation);
-/*router.get('/api/puppies', db.getAllPuppies);
-router.get('/api/puppies/:id', db.getSinglePuppy);*/
-/*router.post('/api/puppies', db.createPuppy);
-router.put('/api/puppies/:id', db.updatePuppy);
-router.delete('/api/puppies/:id', db.removePuppy);*/
-
 
 module.exports = router;
